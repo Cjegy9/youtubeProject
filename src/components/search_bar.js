bb0de9c14b717f8dc0c34717537fce7e801ba677
Colin Eyster <colin.eyster@onespace.com>
2017-04-17 17:01:08 -0500

    Initial Commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: 'text'
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          //onChange={(e) => this.setState({term: e.target.value})}
+        />
+      </div>
+    );
+  }
+
+  onInputChange(e) {
+    console.log(e.target.value);
+  }
+}
+
+export default SearchBar;
